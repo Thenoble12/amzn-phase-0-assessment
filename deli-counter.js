0560f4aa52ff99a28ff3d43b8cb95d8d3a563455
Elijah Noble <elijahnobledev@gmail.com>
2022-04-09 18:51:17 -0700

    Completed assignment

--- deli-counter.js
+++ deli-counter.js
@@ -3,13 +3,45 @@
 
 // 2. Example Usage
 
-// const katzDeli = []
+ const katzDeli = []
+
+ function takeANumber(arr, customer) {
+     arr.push(customer);
+     console.log(`Welcome, ${customer}. You are number ${katzDeli.length} in line`)
+ }
 
 // takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
 // takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
 // takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
 
 // line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+function line(arr) {
+    console.log("TEST: " + arr)
+    output = ''
+    if (arr.length){
+        for (let i=0; i < arr.length; i++){    
+            output += `${i+1}. ${arr[i]} `
+            //console.log("TEST: " + output)
+        }        
+    }
+    else{
+        console.log(output = "Empty!")
+    }
+    console.log(`The line is currently: ${output}`)
+}
+
+function nowServing(katzDeli) {
+    let current = katzDeli.shift()
+    console.log(`Currently serving ${current}.`)
+}
+
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+
+line(katzDeli)
+
+nowServing(katzDeli)
 
 // nowServing(katzDeli) //=> "Currently serving Ada."
 
